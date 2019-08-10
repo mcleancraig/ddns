632f7119060a4793212816132c144bfe13cb6f7d
Craig McLean <mcleancraig@users.noreply.github.com>
2019-08-10 23:50:41 +0100

    removed a lot of crap, updated functions to work more efficiently, probably broke stuff

--- ddns-aws.sh
+++ ddns-aws.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
 
-# NOTE: You must edit the next 4 lines!!
-IFACE="eno1"
+# Set yo shit up!!
 APIKEY="7a4gElr2E9mIwnMwLbSj"
-ZONE="fukka.co.uk"
-AWSZONE="Z159W85ZCGY57D"
-RECORD="fukka.co.uk"
+ZONE="fukka.co.uk" # Zone according to nsone
+AWSZONE="Z159W85ZCGY57D" # Zone according to AWS
+RECORD="fukka.co.uk" # Record you want to change within the zone
+IP_FINDER_LIST="http://bot.whatismyipaddress.com" "http://ipinfo.io/ip"
+resolver=aws
+
+# To Do:
+# Check current ip is valid, external IP - DONE
+# check it isn't the currently registered IP
+# Register it if needed
+
+# Rename the functions to something more descriptive - DONE
 
 #copied from http://www.linuxjournal.com/content/validating-ip-address-bash-script
-function valid_ip()
+
+function bail() {
+ echo "Bailing out:$1"
+ exit 1
+ }
+
+function debug() {
+debug=true && echo "DEBUG: $1"
+}
+
+function is_valid_ip()
 {
   local ip=$1
-  local stat=1
+  local stat=1 # set to fail to danger
 
   if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
     OIFS=$IFS
@@ -25,58 +43,47 @@ function valid_ip()
 }
 
 #inspired by https://unix.stackexchange.com/questions/98923/programmatically-extract-private-ip-addresses
-function public_ip()
+function is_public_ip()
 {
   local ip=$1
-  local stat=2
-
+  local stat=2 # should never fail with this code
+  
   echo $ip | grep -q -E '^(192\.168|10\.|172\.1[6789]\.|172\.2[0-9]\.|172\.3[01]\.)'
   stat=$?
   return $stat
 }
 
-function check_ip()
-{
-  valid_ip $1
-  if [ $? -ne 0 ]; then
-    echo "Couldn't find a valid IP address, your interface might be down or you failed to fill out the 4 obvious lines at the top"
-    exit 1
-  fi
-}
+#Rewrite mode...
 
 # Get my public IP
-
-#IPDATA=`/sbin/ifconfig $IFACE | egrep "^[[:space:]]+inet addr:"`
-#if [ -z $IPDATA ]; then
-#  IPDATA=`/sbin/ifconfig $IFACE | egrep "^[[:space:]]+inet "`
-#fi
-#IPADDR=`echo $IPDATA | awk {'print $2'} | cut -d : -f 2`
-IPADDR=$(wget -qO- http://bot.whatismyipaddress.com)
-
-# Check it's valid
-check_ip $IPADDR
-
-# Check it's not a non-routable IP, if it is, then try getting my public IP from a different source
-public_ip $IPADDR
-if [ $? -eq 2 ]; then
-  echo "This should never happen..."
-  exit 2
-elif [ $? -eq  1 ]; then
-  IPADDR=`curl -s http://ipinfo.io/ip`
-  check_ip $IPADDR
-fi
+for address in ${IP_FINDER_LIST}
+do
+  debug "checking from ${finder}"
+  IPADDR=$(wget -qO- ${finder})
+  debud "returned ${IPADDR}"
+  is_valid_ip ${IPADDR} && is_public_ip ${IPADDR} && break
+done
 
 # Do the do...
+case resolver in 
+ "nsone")
+# NSOne version
+curl -X POST -H "X-NSONE-Key: $APIKEY" -d '{
+ "answers": [
+  {
+   "answer": [
+    "'$IPADDR'"
+   ]
+  }
+ ]
+}' https://api.nsone.net/v1/zones/$ZONE/$RECORD/A ;;
+ "aws")
+# AWS version 
+ aws route53 change-resource-record-sets --hosted-zone-id ${AWSZONE} --change-batch '{ "Comment": "Testing update of a record", "Changes": [ { "Action": "UPSERT", "ResourceRecordSet":{ "Name": "'$RECORD'", "Type": "A", "TTL": 100, "ResourceRecords": [ { "Value": "'$IPADDR'" } ] } } ] }' ;;
+  *) bail "Invalid resolver specified: ${resolver}" ;;
+esac
 
-#curl -X POST -H "X-NSONE-Key: $APIKEY" -d '{
-# "answers": [
-#  {
-#   "answer": [
-#    "'$IPADDR'"
-#   ]
-#  }
-# ]
-#}' https://api.nsone.net/v1/zones/$ZONE/$RECORD/A
-#
-# Updated to AWS version 
- aws route53 change-resource-record-sets --hosted-zone-id ${AWSZONE} --change-batch '{ "Comment": "Testing update of a record", "Changes": [ { "Action": "UPSERT", "ResourceRecordSet":{ "Name": "'$ZONE'", "Type": "A", "TTL": 100, "ResourceRecords": [ { "Value": "'$IPADDR'" } ] } } ] }'
+ 
+ 
+ 
+ 
