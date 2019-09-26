b0685ea8e97e816a3e2d6debb8c66af4323dd20e
mcleancraig <972a4356135a9c44048d8ce902a0572f621cb105>
2019-09-26 17:21:04 +0100

    going home

--- src/ddns.go
+++ src/ddns.go
@@ -2,37 +2,37 @@ package main
 
 import (
 	"fmt"
-	"runtime"
 	"github.com/spf13/viper"
+	"runtime"
 )
-var debug = true
 
-func main()  {
-	f_debug("In function")
+//var debug = true
+
+func main() {
 	getConfig()
-}
-func funcName() string {
-	caller, _, _, _ := runtime.Caller(1)
-	return runtime.FuncForPC(caller).Name()
+	f_debug("In function")
 }
 
 func getConfig() {
-	f_debug("In function")
 	viper.SetConfigName("ddns")
-	viper.AddConfigPath("~/.ddns")
+	viper.AddConfigPath("$HOME/.ddns/")
+	viper.SetDefault("debug", true)
+	f_debug("In function")
 	err := viper.ReadInConfig()
-	if err != nil { bail("err")}
+	if err != nil {
+		bail(err)
+	}
 }
 
-func bail(msg string) {
+func bail(msg error) {
 	f_debug("In function")
 	panic(fmt.Errorf("Fatal error : %s \n", msg))
 }
 
 func f_debug(msg string) {
-	if debug == true {
+	if viper.Get("debug") == true {
 		caller, _, _, _ := runtime.Caller(1)
 		cname := runtime.FuncForPC(caller).Name()
-		fmt.Println("DEBUG FROM: " + cname + " : " + msg )
+		fmt.Println("DEBUG FROM: " + cname + " : " + msg)
 	}
 }
