2b99a17d9763529ee55559ae5ad9aaab85c42233
mcleancraig <972a4356135a9c44048d8ce902a0572f621cb105>
2019-09-27 11:48:00 +0100

    Updated a load of stuff, reworked var names, tidied up functions, skipped assigning responses to vars where not needed

--- src/ddns.go
+++ src/ddns.go
@@ -3,17 +3,42 @@ package main
 import (
 	"fmt"
 	"github.com/spf13/viper"
+	"io/ioutil"
+	"net"
+	"net/http"
 	"runtime"
+	"strings"
 )
 
-//var debug = true
-
-func main() {
-	getConfig()
+func f_debug(msg string) {
+	if viper.Get("debug") == true {
+		caller, _, _, _ := runtime.Caller(1)
+		cname := runtime.FuncForPC(caller).Name()
+		fmt.Println("DEBUG FROM: " + cname + " : " + msg)
+	}
+}
+func bail(msg error) {
 	f_debug("In function")
+	panic(fmt.Errorf("Fatal error : %s \n", msg))
 }
 
-func getConfig() {
+func main() {
+	fGetConfig()
+	f_debug("In function")
+	//currentDns := fGetCurrentDns()
+	//f_debug("Current DNS passed back is: " + fGetCurrentDns())
+	//currentIP: = fGetCurrentIp()
+	//f_debug("current reported IP is: " + fGetCurrentIp())
+	//fChangeIfNeeded()
+
+	if fGetCurrentDns() == fGetCurrentIp() {
+		f_debug("IPs match - not changing record")
+	} else {
+		f_debug("call out to change function")
+	}
+}
+
+func fGetConfig() {
 	viper.SetConfigName("ddns")
 	viper.AddConfigPath("$HOME/.ddns/")
 	viper.SetDefault("debug", true)
@@ -24,15 +49,43 @@ func getConfig() {
 	}
 }
 
-func bail(msg error) {
+func fGetCurrentIp() string {
+	// need to spot bullshit responses from here...
 	f_debug("In function")
-	panic(fmt.Errorf("Fatal error : %s \n", msg))
+	var (
+		//body   string
+		finder = viper.GetStringSlice("ip_finder")
+	)
+	fmt.Printf("Type: %T,  Size: %d \n", finder, len(finder))
+	for i, v := range finder {
+		fmt.Printf("Index: %d, Value: %v\n", i, v)
+		resp, err := http.Get(v)
+		if err != nil {
+			bail(err)
+		} else {
+			defer resp.Body.Close()
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				bail(err)
+			}
+			_ = resp.Body.Close()
+			f_debug("Current IP address reported as: " + string(body))
+			return string(body)
+
+		}
+	}
+	return "Failed to get IP"
 }
 
-func f_debug(msg string) {
-	if viper.Get("debug") == true {
-		caller, _, _, _ := runtime.Caller(1)
-		cname := runtime.FuncForPC(caller).Name()
-		fmt.Println("DEBUG FROM: " + cname + " : " + msg)
-	}
+func fGetCurrentDns() string {
+	// need to do authoritative lookup here, avoid cache
+	f_debug("In function")
+	current_dns, _ := net.LookupHost(viper.GetString("record"))
+	f_debug("Current DNS entry: " + strings.Join(current_dns, "."))
+	return strings.Join(current_dns, ".")
+
+}
+
+func fChangeIfNeeded() {
+
 }
